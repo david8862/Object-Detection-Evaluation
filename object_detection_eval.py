bcd935aafa6e7197da3242d572db474e4fb3be4c
Xiaobin Zhang <david8862@gmail.com>
2021-06-04 11:29:42 +0800

    minor update

--- object_detection_eval.py
+++ object_detection_eval.py
@@ -20,18 +20,6 @@ def get_classes(classes_path):
     class_names = [c.strip() for c in class_names]
     return class_names
 
-def get_colors(class_names):
-    # Generate colors for drawing bounding boxes.
-    hsv_tuples = [(x / len(class_names), 1., 1.)
-                  for x in range(len(class_names))]
-    colors = list(map(lambda x: colorsys.hsv_to_rgb(*x), hsv_tuples))
-    colors = list(
-        map(lambda x: (int(x[0] * 255), int(x[1] * 255), int(x[2] * 255)),
-            colors))
-    np.random.seed(10101)  # Fixed seed for consistent colors across runs.
-    np.random.shuffle(colors)  # Shuffle colors to decorrelate adjacent classes.
-    np.random.seed(None)  # Reset seed to default.
-    return colors
 
 def get_dataset(annotation_file, shuffle=True):
     with open(annotation_file) as f:
@@ -43,49 +31,6 @@ def get_dataset(annotation_file, shuffle=True):
         np.random.seed(None)
 
     return lines
-
-def draw_label(image, text, color, coords):
-    font = cv2.FONT_HERSHEY_PLAIN
-    font_scale = 1.
-    (text_width, text_height) = cv2.getTextSize(text, font, fontScale=font_scale, thickness=1)[0]
-
-    padding = 5
-    rect_height = text_height + padding * 2
-    rect_width = text_width + padding * 2
-
-    (x, y) = coords
-
-    cv2.rectangle(image, (x, y), (x + rect_width, y - rect_height), color, cv2.FILLED)
-    cv2.putText(image, text, (x + padding, y - text_height + padding), font,
-                fontScale=font_scale,
-                color=(255, 255, 255),
-                lineType=cv2.LINE_AA)
-    return image
-
-
-def draw_boxes(image, boxes, classes, scores, class_names, colors, show_score=True):
-    if classes is None or len(classes) == 0:
-        return image
-
-    for box, cls, score in zip(boxes, classes, scores):
-        xmin, ymin, xmax, ymax = box
-
-        class_name = class_names[cls]
-        if show_score:
-            label = '{} {:.2f}'.format(class_name, score)
-        else:
-            label = '{}'.format(class_name)
-        #print(label, (xmin, ymin), (xmax, ymax))
-
-        # if no color info, use black(0,0,0)
-        if colors == None:
-            color = (0,0,0)
-        else:
-            color = colors[cls]
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 1, cv2.LINE_AA)
-        image = draw_label(image, label, color, (xmin, ymin))
-
-    return image
 
 
 def annotation_parse(annotation_lines, class_names):
